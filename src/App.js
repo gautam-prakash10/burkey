814e665219a0ce14f7db1f26fdfc88b295ff83b1
gautam-prakash10 <124663498+gautam-prakash10@users.noreply.github.com>
2023-12-18 23:00:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
+import Menu from "./components/Menu";
 import Getintouch from "./components/Getintouch";
-//import Header from "./components/Header";
+import Header from "./components/Header";
+import About from "./components/About";
+import Order from "./components/Order";
+import Gallary from "./components/Gallary";
+import Review from "./components/Review";
 
 export default function App() {
   return (
     <div className="">
-      {/* <Header/> */}
+      <Header/>
       <Banner/>
+      <About/>
+      <Menu/>
+      <Order/>
+      <Gallary/>
+      <Review/>
       <Getintouch/>
       <Footer/>
     </div>
