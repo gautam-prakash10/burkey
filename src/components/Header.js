c42364b4c322e2b2212fd2da225a6f1c41951180
gautam-prakash10 <124663498+gautam-prakash10@users.noreply.github.com>
2023-12-18 22:58:31 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-        <Header class="justify-items-center">
-            <img src='https://burkey.in/wp-content/uploads/2023/06/burkey-logo.png' alt=''></img>
-        </Header>
-      
+    <div className='bg-black fixed w-full top-0 z-50'>
+      <div className='pt-6 flex justify-center'>
+        <img
+          src='https://burkey.in/wp-content/uploads/2023/06/burkey-logo.png'
+          className='mx-auto'
+          alt='header'
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
