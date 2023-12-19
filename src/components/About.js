d1908a18c6d5b088d9e5d76f8de22fcc89a30a21
Mansvi0802 <127412166+Mansvi0802@users.noreply.github.com>
2023-12-19 20:27:12 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import React from 'react';
 import "./About.css"
 
 const About = () => {
@@ -9,15 +7,15 @@ const About = () => {
           <div className="flex flex-row">
           <div className="flex flex-column">
           <div className='flex-1 ml-10 mt-10 items-left'>
-          <p className='text-5xl text-yellow-400 mb-5'>About Burkey</p>
-          <p className='text-xl text-white mb-5 mt-5'>Burkey - Niwala Burkey delivers authentic and Delectable North Indian and Mughali Cuisine.
+          <p className='text-5xl text-yellow-400 mb-5 mt-20'>About Burkey</p>
+          <p className='text-xl text-white mb-5 mt-7'>Burkey - Niwala Burkey delivers authentic and Delectable North Indian and Mughali Cuisine.
            As the name symbolises, our mouth watering Punjabi Niwala will leave your taste buds carving for another Burkey. 
           The true art of North Indian cooking lies in the subtle use and variation of spices which makes each dish different and exotic
           </p>
-          <button className='bg-white hover:bg-gray-200 text-black-500 py-2 px-4 rounded-3xl'>Know More</button>
+          <button className='bg-white hover:bg-gray-200 text-black-500 py-2 px-4 font-semi-bold rounded-3xl'>Know More</button>
           </div>
 
-          <div className="flex-1 mt-10 ml-10 items-right">
+          <div className="flex-1 mt-10 ml-10 w-200 h-200 items-right">
         <img src="https://burkey.in/wp-content/uploads/2023/06/Gif-Burkey-1.gif" alt="food-gif" />
       </div>
           </div>
@@ -25,8 +23,5 @@ const About = () => {
       </div>
       )
   }
-    
-    export default About
-  };
     
     export default About
