f639f20b0edf532147788b458485c3f95530b42f
Mansvi0802 <127412166+Mansvi0802@users.noreply.github.com>
2023-12-20 13:20:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Order from "./components/Order";
 import Gallery from "./components/Gallery";
-import Reviews from "./components/Reviews";
+import Sliding from "./components/Reviews";
 
 export default function App() {
   return (
@@ -17,8 +17,21 @@ export default function App() {
       <About/>
       <Menu/>
       <Order/>
-      <Gallary/>
-      <Review/>
+      <Gallery/>
+      <div className="grid place-items-center mt-10">
+            <h2 className="text-center text-4xl text-yellow-400 mb-10">
+              Instagram
+            </h2>
+              <button className="px-4 py-2 bg-yellow-400 text-white rounded-md">
+                Follow us
+              </button>
+          </div>
+      <div className="bg-black">
+        <h2 className=" text-4xl font-bold text-yellow-400 mb-10 mt-10 text-center">
+          Google Reviews
+        </h2>
+      </div>
+      <Sliding />
       <Getintouch/>
       <Footer/>
     </div>
