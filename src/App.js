0101620c0ce4ff9cdf1bf989d779f68e2c27a871
gautam-prakash10 <124663498+gautam-prakash10@users.noreply.github.com>
2023-12-20 22:18:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Getintouch from "./components/Getintouch";
 import Header from "./components/Header";
 import About from "./components/About";
 import Order from "./components/Order";
-import Gallery from "./components/Gallery";
-import Sliding from "./components/Reviews";
+import Gallary from "./components/Gallary";
+// import Review from "./components/Review";
+import Sliding from "./components/Sliding";
 
 export default function App() {
   return (
     <div className="">
       <Header/>
       <Banner/>
+      <div className="bg-black">
+
       <About/>
       <Menu/>
       <Order/>
-      <Gallery/>
+      <Gallary/>
       <div className="grid place-items-center mt-10">
-            <h2 className="text-center text-4xl text-yellow-400 mb-10">
+            <h2 className="text-center font-serif text-4xl text-yellow-400 mb-10">
               Instagram
             </h2>
               <button className="px-4 py-2 bg-yellow-400 text-white rounded-md">
@@ -27,11 +30,12 @@ export default function App() {
               </button>
           </div>
       <div className="bg-black">
-        <h2 className=" text-4xl font-bold text-yellow-400 mb-10 mt-10 text-center">
+        <h2 className=" text-4xl font-bold font-serif text-yellow-400 mb-10 mt-10 text-center">
           Google Reviews
         </h2>
       </div>
-      <Sliding />
+      <Sliding/>
+      </div>
       <Getintouch/>
       <Footer/>
     </div>
