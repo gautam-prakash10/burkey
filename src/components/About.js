0e2d5fc98dfeafe93785f2814aac547aff96803d
gautam-prakash10 <124663498+gautam-prakash10@users.noreply.github.com>
2023-12-20 22:13:43 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
     <div className="flex flex-col items-center bg-fuchsia-950">
       <div className="flex flex-col-reverse md:flex-row w-full max-w-screen-xl">
         {/* Left Section (Text) */}
-        <div className="flex-1 p-5 md:p-10 items-center">
-          <p className="text-4xl text-yellow-400 mb-5 font-serif mt-10 md:mt-0">About Burkey</p>
+        <div className="flex-1 p-5 md:p-10 items-center justify-center">
+          <p className="text-4xl text-yellow-400 mb-5  font-serif mt-10 md:mt-0">About Burkey</p>
           <p className="font-sans text-lg md:text-xl text-white mb-5 mt-5">
             Burkey - Niwala Burkey delivers authentic and delectable North Indian and Mughali Cuisine. As the name symbolizes, our mouth-watering Punjabi Niwala will leave your taste buds craving for another Burkey. The true art of North Indian cooking lies in the subtle use and variation of spices which makes each dish different and exotic.
           </p>
